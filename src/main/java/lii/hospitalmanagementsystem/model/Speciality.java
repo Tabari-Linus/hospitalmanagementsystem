c33359ebf1c242398ebd70918e425ac523287fb5
Mr Lii <leetamparega@gmail.com>
2025-05-10 00:57:24 +0000

    docs: add Speciality model

--- src/main/java/lii/hospitalmanagementsystem/model/Speciality.java
+++ src/main/java/lii/hospitalmanagementsystem/model/Speciality.java
@@ -1,1 +1,33 @@
 package lii.hospitalmanagementsystem.model;
+public class Speciality {
+    private Long specialityId;
+    private String name;
+
+    public Speciality() {}
+
+    public Speciality(Long specialityId, String name) {
+        this.specialityId = specialityId;
+        this.name = name;
+    }
+
+    public Long getSpecialityId() {
+        return specialityId;
+    }
+
+    public void setSpecialityId(Long specialityId) {
+        this.specialityId = specialityId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
