c4d137520fbe68481f2af9fa0b9017c8a5338d6b
Mr Lii <leetamparega@gmail.com>
2025-05-10 04:42:41 +0000

    config: export lii.hospitalmanagementsystem.view in module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -15,4 +15,8 @@ module lii.hospitalmanagementsystem {
     exports lii.hospitalmanagementsystem;
     exports lii.hospitalmanagementsystem.controller;
     opens lii.hospitalmanagementsystem.controller to javafx.fxml;
+    exports lii.hospitalmanagementsystem.model;
+    opens lii.hospitalmanagementsystem.model to javafx.fxml;
+    exports lii.hospitalmanagementsystem.view;
+    opens lii.hospitalmanagementsystem.view to javafx.fxml;
 }
