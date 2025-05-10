cd032b9558799428855e2fb0fe10db1b7be0b948
Mr Lii <leetamparega@gmail.com>
2025-05-10 00:34:04 +0000

    docs: add Nurse model

--- src/main/java/lii/hospitalmanagementsystem/model/Nurse.java
+++ src/main/java/lii/hospitalmanagementsystem/model/Nurse.java
@@ -1,4 +1,61 @@
 package lii.hospitalmanagementsystem.model;
 
+import java.math.BigDecimal;
+
 public class Nurse {
+    private long employeeId;
+    private String rotation;
+    private double salary;
+    private long departmentId;
+
+    public Nurse() {}
+
+    public Nurse(long employeeId, String rotation, double salary, long departmentId) {
+        this.employeeId = employeeId;
+        this.rotation = rotation;
+        this.salary = salary;
+        this.departmentId = departmentId;
+    }
+
+    public long getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getRotation() {
+        return rotation;
+    }
+
+    public void setRotation(String rotation) {
+        this.rotation = rotation;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public long getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(long departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    @Override
+    public String toString() {
+        return "Nurse{" +
+                "employeeId=" + employeeId +
+                ", rotation='" + rotation + '\'' +
+                ", salary=" + salary +
+                ", departmentId=" + departmentId +
+                '}';
+    }
 }
