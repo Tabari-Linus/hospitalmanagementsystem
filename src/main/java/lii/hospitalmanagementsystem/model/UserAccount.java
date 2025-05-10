7bf52d14d55dcf50329a7557bceb093348a44b84
Mr Lii <leetamparega@gmail.com>
2025-05-10 00:57:58 +0000

    docs: add UserAccount model

--- src/main/java/lii/hospitalmanagementsystem/model/UserAccount.java
+++ src/main/java/lii/hospitalmanagementsystem/model/UserAccount.java
@@ -1,4 +1,71 @@
 package lii.hospitalmanagementsystem.model;
 
+
 public class UserAccount {
+    private long userId;
+    private String username;
+    private String password;
+    private boolean isAdmin;
+    private Long employeeId;
+
+    public UserAccount() {}
+
+    public UserAccount(long userId, String username, String password, boolean isAdmin, Long employeeId) {
+        this.userId = userId;
+        this.username = username;
+        this.password = password;
+        this.isAdmin = isAdmin;
+        this.employeeId = employeeId;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+
+    public Long getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(Long employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    @Override
+    public String toString() {
+        return "UserAccount{" +
+                "userId=" + userId +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", isAdmin=" + isAdmin +
+                ", employeeId=" + employeeId +
+                '}';
+    }
 }
