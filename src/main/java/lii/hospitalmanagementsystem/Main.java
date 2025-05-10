5828f769690443dbffe187bb766f06829140f9fd
Mr Lii <leetamparega@gmail.com>
2025-05-10 04:25:44 +0000

    docs: update Main to add fxml for loading

--- src/main/java/lii/hospitalmanagementsystem/Main.java
+++ src/main/java/lii/hospitalmanagementsystem/Main.java
@@ -1,23 +1,35 @@
+// Main.java
 package lii.hospitalmanagementsystem;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-import java.io.IOException;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/lii/hospitalmanagementsystem/view/MainDashboard.fxml"));
+        Scene scene = new Scene(loader.load(), 1200, 800); // Set window size
+        scene.getStylesheets().add(getClass().getResource("/lii/hospitalmanagementsystem/css/styles.css").toExternalForm());
+
+        stage.setTitle("Hospital Management System");
         stage.setScene(scene);
+        stage.setMinWidth(800);
+        stage.setMinHeight(600);
+
+        // Center the window on screen
+        stage.centerOnScreen();
+
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.show();
+
+        // Make window maximized by default
+//        stage.setMaximized(true);
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
